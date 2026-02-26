62e39bafccdab9cb9ae30563d5f13f3dc4ab4ee6
OratilweBotlhale <botlhaleradikeledi@gmail.com>
2026-02-26 15:00:16 +0200

    Add files via upload

---
+++ explore_data.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+#load dataset
+# without encoding, youll get an
+df = pd.read_csv("data/McDonald_s_Reviews.csv", encoding="latin1")
+
+#preview the first 5 rows
+print(df.head())
+
+#dataset info
+print("\nDataset Info:")
+print(df.info())
+
+#column name
+print("\nColumn names:")
+print(df.columns)
+
+#check rating
+print("\nRating Distribution: ")
+print(df['rating'].value_counts())
